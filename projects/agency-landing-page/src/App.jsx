6b00d1d3416a2d4b79da6855b33cf97c10492348
coronelhernan <hcoronel1996@gmail.com>
2026-02-06 20:18:30 -0300

    chore: configuration initial agency-landing-page

---
+++ projects/agency-landing-page/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-500">
+        Hello World
+      </h1>
+    </div>
+  )
+}
