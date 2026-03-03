2e07b6f3848a846e4fc015406ff34cccdbdfcfa5
coronelhernan <hcoronel1996@gmail.com>
2026-03-03 20:02:49 -0300

    feat(rest-countries): complete router version with country detail navigation

--- projects/rest-countries-app/src/components/sections/Navbar.jsx
+++ projects/rest-countries-app/src/components/sections/Navbar.jsx
@@ -2,29 +2,33 @@ import { Moon } from "lucide-react";
 import { useTheme } from "../../hooks/useTheme";
 
 export default function Navbar() {
-  const { toggleTheme } = useTheme();
-  
+  const { toggleTheme, isDark } = useTheme();
+
   return (
     <nav
       className="bg-[var(--color-surface)] text-[var(--color-text)] 
       h-22 w-full flex justify-between items-center px-4
       lg:px-18 lg:h-20 shadow-sm"
     >
-      <h1 className="text-base font-bold lg:text-2xl">
-        Where in the world?
-      </h1>
+      <h1 className="text-base font-bold lg:text-2xl">Where in the world?</h1>
 
       <div className="flex gap-2 items-center">
         <button
           onClick={toggleTheme}
           className="p-2 rounded-full hover:opacity-80 transition"
         >
-          <Moon size={24} className="text-[var(--color-text)]" />
+          {isDark ? (
+            <Moon size={24} 
+            fill="currentColor"
+            className="text-[var(--color-text)]" />
+          ) : (
+            <Moon size={24} 
+            fill="none"
+            className="text-[var(--color-text)]" />
+          )}
         </button>
 
-        <span className="text-base">
-          Dark mode
-        </span>
+        <span className="text-base">Dark mode</span>
       </div>
     </nav>
   );
